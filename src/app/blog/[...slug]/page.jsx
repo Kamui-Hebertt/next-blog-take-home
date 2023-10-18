4fe4903ec8c972b898742a918abdb50a1346283c
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-10-18 20:22:09 -0300

    fix: data time timezone

--- src/app/blog/[...slug]/page.jsx
+++ src/app/blog/[...slug]/page.jsx
@@ -2,6 +2,7 @@
 import { Post } from '@/Interfaces/Iapi';
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
+import styles from './details.module.css';
 
 export default function Page({ params }) {
   const [post, setPost] = useState(null);
@@ -20,24 +21,38 @@ export default function Page({ params }) {
     getPost();
   }, [params.slug[0]]);
 
+  const formatBrazilianDate = (dateString) => {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    const date = new Date(dateString);
+    
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    return date.toLocaleDateString('pt-BR', options);
+  };
+
   return (
-    <div>
-      {post ? (
-        <div>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          <Image
-            src={post.imageUrl} 
-            alt={post.title}
-            width={500}
-            height={500}
-      />
-          <p>{post.date}</p>
-       
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <div className={styles.pagecontainer}>
+    {post ? (
+      <div className={styles.container}>
+        <h2 className={styles.title}>{post.title}</h2>
+        <p>{post.content}</p>
+        <Image
+          className={styles.postimage}
+          src={post.imageUrl}
+          alt={post.title}
+          width={500}
+          height={500}
+        />
+       <p className={styles.date}>
+            Publicado em {formatBrazilianDate(post.date)}
+          </p>
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
   );
 }
